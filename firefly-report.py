feaf19a90b349c948c59c78caaf43cdc34121053
Matthias Eck <matthias_eck@icloud.com>
2025-04-22 22:16:42 +0200

    feat(report): make inputs prettier

--- firefly-report.py
+++ firefly-report.py
@@ -8,9 +8,35 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    import pandas as pd
+    import marimo as mo
 
-    df = pd.read_csv("../2025_04_21_transaction_export.csv")
+    app_title = mo.md("# Firefly Reporting")
+    import_title = mo.md("## Import Transactions")
+
+    file = mo.ui.file(
+        label="Select Transaction CSV",
+        filetypes=[".csv"],
+        kind="button"
+    )
+
+    mo.vstack([app_title, import_title, file])
+    return file, mo
+
+
+@app.cell
+def _(file):
+    import pandas as pd
+    import io
+
+    if file.value is not None:
+        try:
+            csv_data = file.value[0].contents.decode('utf-8')
+            df = pd.read_csv(io.StringIO(csv_data))
+        except Exception as e:
+            print(f"Error reading CSV data: {e}")
+            df = pd.DataFrame()
+    else:
+        df = pd.DataFrame()  # Initialize an empty DataFrame if no file is selected
     return df, pd
 
 
@@ -18,7 +44,8 @@ def _():
 def _(df, pd):
     df['account_name'] = df['source_name']
     df['account_type'] = df['source_type']
-    df['corrected_amount'] = -abs(df['amount'])
+    df['leg_amount'] = -abs(df['amount'])
+    df['leg_type'] = "Source"
     df['date'] = pd.to_datetime(df['date'], utc=True)
     return
 
@@ -31,7 +58,8 @@ def _(df, pd):
     # Override account_name and account_type with destination_name and destination_type
     _df_duplicated['account_name'] = _df_duplicated['destination_name']
     _df_duplicated['account_type'] = _df_duplicated['destination_type']
-    _df_duplicated['corrected_amount'] = abs(_df_duplicated['amount'])
+    _df_duplicated['leg_amount'] = abs(_df_duplicated['amount'])
+    _df_duplicated['leg_type'] = "Destination"
 
     # Concatenate the original and modified DataFrames
     df_enhanced = pd.concat([df, _df_duplicated], ignore_index=True)
@@ -39,139 +67,96 @@ def _(df, pd):
 
 
 @app.cell
-def _(df_enhanced):
-    import marimo as mo
+def _(df_enhanced, mo):
+    def get_column_values(column_name):
+        _column_values = df_enhanced[column_name].unique()
+        _column_values = [value for value in _column_values if isinstance(value, str)]
+        _column_values.sort()
+    
+        return _column_values
 
-    categories = df_enhanced["category"].unique()
-    categories = [c for c in categories if isinstance(c, str)]
-    categories.sort()
+    def get_column_selector(column_name):
+        _column_values = get_column_values(column_name)
+    
+        return mo.ui.multiselect(options=_column_values)
 
-    tags = df_enhanced["tags"].unique()
-    tags = [c for c in tags if isinstance(c, str)]
-    tags.sort()
+    def get_column_selector_type():
+        return mo.ui.radio(options=["Include", "Exclude"], inline=True)
 
-    types = df_enhanced["type"].unique()
-    types = [c for c in types if isinstance(c, str)]
-    types.sort()
+    def get_column_array(column_name):
+        return mo.ui.array([
+            get_column_selector_type(),
+            get_column_selector(column_name),
+        ])
 
-    account_types = df_enhanced["account_type"].unique()
-    account_types = [at for at in account_types if isinstance(at, str)]
-    account_types.sort()
+    def get_column_array2(column_name_base, column_name_base_item, column_name_derived):
+        _column_values = df_enhanced[df_enhanced[column_name_base] == column_name_base_item][column_name_derived].unique()
+        _column_values = [value for value in _column_values if isinstance(value, str)]
+        _column_values.sort()
 
+    category = get_column_array("category")
+    tag = get_column_array("tags")
+    type = get_column_array("type")
 
-    category_selector = mo.ui.dropdown(
-        options=categories,
-        label="Category",
-    )
+    #account_name_dict2 = mo.ui.dictionary({ account_type: get_column_array2("account_type", account_type, "account_name") for account_type in get_column_values("account_type") })
+    account_name_dict2 = mo.ui.dictionary({})
 
-    tag_selector = mo.ui.dropdown(
-        options=tags,
-        label="Tag",
-    )
-
-    type_selector = mo.ui.dropdown(
-        options=types,
-        label="Type",
-    )
-
-    account_name_selectors = {}
-    for account_type in account_types:
+    account_name_dict = mo.ui.dictionary({})
+    for account_type in get_column_values("account_type"):
         account_names = df_enhanced[df_enhanced["account_type"] == account_type]["account_name"].unique()
         account_names = [name for name in account_names if isinstance(name, str)]
         account_names.sort()
-        account_name_selectors[account_type] = mo.ui.dropdown(
-            options=account_names,
-            label=f"Account Name ({account_type})",
-        )
+        account_name_dict.elements[account_type] = mo.ui.array([
+            get_column_selector_type(),
+            mo.ui.multiselect(options=account_names)
+        ])
 
-    account_type_selector = mo.ui.dropdown(
-        options=account_types,
-        label="Account Type",
-    )
-    return (
-        account_name_selectors,
-        account_type_selector,
-        category_selector,
-        mo,
-        tag_selector,
-        type_selector,
-    )
-
-
-@app.cell
-def _(
-    account_name_selectors,
-    account_type_selector,
-    category_selector,
-    mo,
-    tag_selector,
-    type_selector,
-):
-    mo.vstack(
-        [
-            category_selector,
-            *[selector for selector in account_name_selectors.values()],
-            tag_selector,
-            type_selector,
-            account_type_selector,
-        ]
-    )
-    return
-
-
-@app.cell
-def _(df_enhanced, mo):
     date_range_selector = mo.ui.date_range(
         value=(df_enhanced["date"].min().date(), df_enhanced["date"].max().date()),
         label="Date Range",
     )
 
-    date_range_selector
-    return
+    amount_range_selector = mo.ui.range_slider(
+        label="Amount Range",
+        start=df_enhanced["leg_amount"].min(),
+        stop=df_enhanced["leg_amount"].max(),
+        show_value=True
+    )
+    return (
+        account_name_dict,
+        account_name_dict2,
+        amount_range_selector,
+        category,
+        date_range_selector,
+        tag,
+        type,
+    )
 
 
 @app.cell
 def _(
-    account_type_selector,
-    category_selector,
+    account_name_dict,
+    account_name_dict2,
+    amount_range_selector,
+    category,
+    date_range_selector,
     mo,
-    tag_selector,
-    type_selector,
+    tag,
+    type,
 ):
-    filter_types = {
-        "category": category_selector,
-        "tag": tag_selector,
-        "type": type_selector,
-        "account_type": account_type_selector,
-    }
+    mo.vstack(
+        [
+            mo.md("## Filter Transactions"),
+            date_range_selector,
+            amount_range_selector,
+            mo.hstack([mo.md("Catgory"), *category], justify="start", gap=5, widths=[1,1,4]),
+            mo.hstack([mo.md("Tag"), *tag], justify="start", gap=5, widths=[1,1,4]),
+            mo.hstack([mo.md("Type"), *type], justify="start", gap=5, widths=[1, 1,4]),
+            *[mo.hstack([mo.md(selector), *items], justify="start", gap=5, widths=[1,1,4]) for selector, items in account_name_dict.items()],
+            *[mo.hstack(selector, justify="start", gap=5) for selector in account_name_dict2.values()],
 
-    filter_selections = mo.ui.multiselect(
-        options=list(filter_types.keys()), label="Select Filters"
+        ]
     )
-
-
-    def filter_uis(selected_filters):
-        uis = []
-        filter_configs = {}
-        for filter_name in selected_filters:
-            selector = filter_types[filter_name]
-            include_exclude = mo.ui.radio(
-                options=["Include", "Exclude"], value="Include", label="Include/Exclude"
-            )
-            uis.append(mo.hstack([selector, include_exclude]))
-            filter_configs[filter_name] = (selector, include_exclude)
-
-        return mo.vstack(*uis), filter_configs
-    return filter_selections, filter_uis
-
-
-@app.cell
-def _(filter_selections, filter_uis, mo):
-    def _filter_ui(filter_selections_value):
-        filter_ui, _filter_configs = filter_uis(filter_selections_value)
-        return filter_ui
-
-    mo.vstack([_filter_ui(filter_selections.value)])
     return
 
 
